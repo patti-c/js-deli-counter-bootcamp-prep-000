eb36bb17f91d88915719c56c9a5247336002fd94
Patricia Carter <patricia4194@gmail.com>
2018-07-24 20:28:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,25 +24,18 @@ console.log(takeANumber(katzDeli));
 
 function currentLine(katzDeli) {
   
-  // It's just a little easier to type 
-  // and we're going to be using this a lot.
   const lineLength = katzDeli.length
   
   if(lineLength === 0) {
     return 'The line is currently empty.';
   } else {
     
-    // First open the list 
     var list = `The line is currently: 1. ${katzDeli[0]}, `;
     
-    // Loop through the middle of the array
-    // attaching commas appropriately 
     for(let i = 1 ; i < lineLength - 1 ; i++) {
       list = list + `${i+1}. ${katzDeli[i]}, `;
     }
     
-    // Attaches the last item seperately so it
-    // doesn't have a comma at the end.
     list = list + `${lineLength}. ${katzDeli[lineLength - 1]}`;
     return list;
   }
