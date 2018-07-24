98fca956837f3c722a796f2f1c62309305ca5b1b
Patricia Carter <patricia4194@gmail.com>
2018-07-24 20:08:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var katzDeli = [];
 
-function takeANumber(katzDeli, name) {
-  katzDeli.push(name);
+function takeANumber(katzDeli) {
+  katzDeli.push(i);
+  i++;
   return `Welcome, ${name}. You are number ${katzDeli.length} in line.`;
 }
 
